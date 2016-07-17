1841e1cd754fbad6c07880f2ab3a7c5168ac362a
Jan Käberich <kaeberic@ibr.cs.tu-bs.de>
2016-07-17 14:00:27 +0200

    Automatically set RTC after flashing
    - Add compile time via makefile (+forced comiler run before flashing)
    - at bootup: compare compile time with EEPROM timestamp
    	- if different: set RTC with compile time and save to EEPROM

--- software/moonlamp/moonlamp.c
+++ software/moonlamp/moonlamp.c
@@ -1,8 +1,37 @@
 #include <avr/io.h>
 
 #include "moon.h"
+#include "eeprom.h"
+#include "ds1307.h"
 
-int main(void){
+int main(void) {
+	struct date date;
+	struct time time;
+	eeprom_Read(&date, &time);
+
+	// compare EEPROM time with compile time
+	if ((date.day != COMPILE_DAY) || (date.month != COMPILE_MONTH)
+			|| (date.year != (COMPILE_YEAR - 2000))
+			|| (time.second != COMPILE_SECOND)
+			|| (time.minute != COMPILE_MINUTE) || (time.hour != COMPILE_HOUR)) {
+		// EEPROM date/time doesn't match compile date/time
+		// -> device has just been flashed, write compile date/time to RTC
+		date.day = COMPILE_DAY;
+		date.month = COMPILE_MONTH;
+		date.year = (COMPILE_YEAR - 2000);
+		time.second = COMPILE_SECOND;
+		time.minute = COMPILE_MINUTE;
+		time.hour = COMPILE_HOUR;
+		// set RTC
+		DS1307_setTime(time);
+		DS1307_setDate(date);
+		// save in EEPROM
+		eeprom_Write(date, time);
+	}
+	// at this point the RTC has either just been set or still contains a valid
+	// time and date (device hasn't been flashed before current start-up)
+
 	moon_init();
-	while(1);
+	while (1)
+		;
 }
